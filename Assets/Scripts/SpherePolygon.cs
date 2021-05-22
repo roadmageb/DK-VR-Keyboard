b29e89022c51aaf8a4120b7be138317b453e75bc
RoadMageB <roadmageb@naver.com>
2021-05-22 19:51:03 +0900

    upgraded learning algorithm

--- Assets/Scripts/SpherePolygon.cs
+++ Assets/Scripts/SpherePolygon.cs
@@ -16,10 +16,11 @@ public class SpherePolygon
     public Dictionary<KeyCode, List<int>> polygons;
 
     public float[,] safeDistance;
-    public Dictionary<KeyCode, List<Vector2>> learningLog;
+    public Dictionary<KeyCode, List<(Vector2 v, float w)>> learningLog;
 
     private float maxStepDistance = 0.5f;
     private float stepDistRate = 1;
+    private float maxPushDistance = 1;
 
     public SpherePolygon (List<Vector2> _vertices, Dictionary<KeyCode, List<int>> _polygons, float[,] _safeDistance)
     {
@@ -57,10 +58,10 @@ public class SpherePolygon
 
     public void InitLearning()
     {
-        learningLog = new Dictionary<KeyCode, List<Vector2>>();
+        learningLog = new Dictionary<KeyCode, List<(Vector2 v, float w)>>();
         foreach(KeyCode k in polygons.Keys)
         {
-            learningLog.Add(k, new List<Vector2>());
+            learningLog.Add(k, new List<(Vector2 v, float w)>());
         }
     }
 
@@ -78,14 +79,14 @@ public class SpherePolygon
         Vector2[] diff = new Vector2[vertices.Count];
         Func<float, float> distMoveFunction = x => 1 - x;
 
-        learningLog[key].Add(pos);
+        learningLog[key].Add((pos, 1f));
 
         foreach(KeyCode k in learningLog.Keys)
         {
-            foreach(Vector2 v in learningLog[k])
+            foreach(var log in learningLog[k])
             {
                 KeyCode outKey;
-                bool clicked = GetPointedKey(v, out outKey);
+                bool clicked = GetPointedKey(log.v, out outKey);
 
                 if (!clicked || outKey != k)
                 {
@@ -93,7 +94,7 @@ public class SpherePolygon
                     float minDist = -1, maxDist = -1;
                     for (int i = 0; i < polygons[k].Count; i++)
                     {
-                        float d = Vector2.Distance(vertices[polygons[k][i]], v);
+                        float d = Vector2.Distance(vertices[polygons[k][i]], log.v);
                         if (minDist < 0 || d < minDist) minDist = d;
                         if (maxDist < 0 || d > maxDist) maxDist = d;
                         distArr[i] = d;
@@ -105,11 +106,27 @@ public class SpherePolygon
                             distArr[i] = (distArr[i] - minDist) / (maxDist - minDist);
                             distArr[i] = distMoveFunction(distArr[i]);
 
-                            Vector2 temp = (v - vertices[polygons[k][i]]) * stepDistRate;
-                            diff[polygons[k][i]] += temp * distArr[i];
+                            Vector2 temp = (log.v - vertices[polygons[k][i]]) * stepDistRate;
+                            diff[polygons[k][i]] += temp * distArr[i] * log.w;
                         }
                     }
                 }
+                else if (outKey == k)
+                {
+                    for (int i = 0; i < polygons[k].Count; i++)
+                    {
+                        float d = Vector2.Distance(vertices[polygons[k][i]], log.v);
+                        if(d < maxPushDistance)
+                        {
+                            Vector2 temp = (vertices[polygons[k][i]] - log.v).normalized * (maxPushDistance - d) * 0.8f;
+                            diff[polygons[k][i]] += temp * log.w;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < learningLog[k].Count; i++)
+            {
+                learningLog[k][i] = (learningLog[k][i].v, learningLog[k][i].w * 0.9f);
             }
         }
         for(int i = 0; i < vertices.Count; i++)
